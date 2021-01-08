7bd845922bad640a2285d0591ec48aa1a751cab1
miswhiramon <yh7500u@gmail.com>
2021-01-08 17:14:25 +0900

    commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const question='ゲーム市場、最も売れたゲーム機は?';
+const answers=[
+    'スーパーファミコン',
+    'プレーステーション2',
+    'ニンテンドースイッチ',
+    'ニンテンドーDS'
+];
+const correct='ニンテンドーDS';
+
+console.log(document.getElementsByTagName('div'));
