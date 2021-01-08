a5ac1dcacb77b355e6b87ddc6a920d5cd46eef84
miswhiramon <yh7500u@gmail.com>
2021-01-08 17:54:17 +0900

    commit

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ const correct='ニンテンドーDS';
 
 console.log(document.getElementById('js-question').textContent);
 document.getElementById('js-question').textContent=question;
-document.getElementsByTagName('button')[0].textContent=answers[0];
-document.getElementsByTagName('button')[1].textContent=answers[1];
-document.getElementsByTagName('button')[2].textContent=answers[2];
-document.getElementsByTagName('button')[3].textContent=answers[3];
+
+const button = document.getElementsByTagName('button');
+button[0].textContent=answers[0];
+button[1].textContent=answers[1];
+button[2].textContent=answers[2];
+button[3].textContent=answers[3];
 
 //クリックしたら正誤判定
 document.getElementsByTagName('button')[0].addEventListener('click',()=>{
