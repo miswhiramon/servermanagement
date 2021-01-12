f9de330fe952fb6fab2a5803574bf302997da2d7
miswhiramon <yh7500u@gmail.com>
2021-01-12 22:40:54 +0900

    commit

--- app.js
+++ app.js
@@ -1,31 +1,157 @@
 const quiz = [
     {
-        question: 'ゲーム市場、最も売れたゲーム機は?',
+        question: 'クラウドコンピューティングの定義について、空欄を選択</br>\
+        ネットワーク、サーバ、ストレージ、アプリケーション、サービスなどの構成可能な\
+        [ X ]の共用プール に対して、便利かつオンデマンドにアクセスでき、\
+        最小の管理労力またはサービスプロバイダ間の相互動作によって\
+        迅速に提供され利用できるという、モデルのひとつである。',
         answers: [
-            'スーパーファミコン',
-            'プレーステーション2',
-            'ニンテンドースイッチ',
-            'ニンテンドーDS'
+            'サーバリソース',
+            '物理マシンリソース',
+            'コンピューティングリソース',
+            '仮想マシンリソース'
         ],
-        correct:'ニンテンドーDS'
+        correct:'コンピューティングリソース'
     },{
-        question: '好きな天気',
+        question: 'クラウドコンピューティングの定義について、空欄を選択</br>\
+        ネットワーク、サーバ、ストレージ、アプリケーション、サービスなどの構成可能な\
+        コンピューティングリソースの共用プール に対して、便利かつオンデマンドにアクセスでき、\
+        最小の管理労力またはサービスプロバイダ間の相互動作によって\
+        [ X ]に提供され利用できるという、モデルのひとつである。',
         answers: [
-            '晴れ',
-            '曇り',
-            '雨',
-            '雪'
+            '安定的',
+            '広範囲',
+            '効率的',
+            '迅速'
         ],
-        correct:'晴れ'
+        correct:'迅速'
     },{
-        question: '休日の過ごし方',
+        question: 'コンピューティングリソースとはなにか？',
         answers: [
-            '家でゴロゴロ',
-            '外で散歩',
-            '買い物',
-            '外食'
+            '通信なども含む計算資源',
+            'コンピュータの性能',
+            '使えるコンピュータの台数',
+            'HDDの記憶容量'
         ],
-        correct:'外食'
+        correct:'通信なども含む計算資源'
+    },{
+        question: 'クラウドでは一人のユーザに対して一つの物理マシンが\
+        </br>割り当てられているわけではない。これを表す言葉は？',
+        answers: [
+            'On demand self service',
+            'Broad network access',
+            'Resource pooling',
+            'Rapid elasticity'
+        ],
+        correct:'Resource pooling'
+    },{
+        question: '複数のストレージを1つに見せるRAIDは[ X ]と高速化が目的である。',
+        answers: [
+            '効率化',
+            '仮想化',
+            '安定化',
+            '冗長化'
+        ],
+        correct:'冗長化'
+    },{
+        question: '会社にデータセンタを用意することを何というか。',
+        answers: [
+            'ホスティング',
+            'オンプレミス型',
+            'IaaS',
+            'クラウド型'
+        ],
+        correct:'オンプレミス型'
+    },{
+        question: '社内ではなく事業者が用意したサーバ環境を使うことを',
+        answers: [
+            'ホスティング',
+            'オンプレミス型',
+            'IaaS',
+            'クラウド型'
+        ],
+        correct:'クラウド型'
+    },{
+        question: 'サーバを物理的構成と切り離した状態で構築することを何というか。',
+        answers: [
+            '効率化',
+            '仮想化',
+            '安定化',
+            '冗長化'
+        ],
+        correct:'仮想化'
+    },{
+        question: 'クラウドコンピューティングは[ X ]がサーバの存在を意識していないことを利用したサービスである。',
+        answers: [
+            '会社',
+            'クラウド事業者',
+            'プロバイダー',
+            'ユーザ'
+        ],
+        correct:'ユーザ'
+    },{
+        question: '',
+        answers: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        correct:''
+    },{
+        question: '',
+        answers: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        correct:''
+    },{
+        question: '',
+        answers: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        correct:''
+    },{
+        question: '',
+        answers: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        correct:''
+    },{
+        question: '',
+        answers: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        correct:''
+    },{
+        question: '',
+        answers: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        correct:''
+    },{
+        question: '',
+        answers: [
+            '',
+            '',
+            '',
+            ''
+        ],
+        correct:''
     }
 ]
 
@@ -36,7 +162,7 @@ let score=0;
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 const setupQuiz = ()=>{
-    document.getElementById('js-question').textContent=quiz[quizIndex].question;
+    document.getElementById('js-question').innerHTML=quiz[quizIndex].question;
     let buttonIndex=0;
     while(buttonIndex<buttonLength){
         $button[buttonIndex].textContent=quiz[quizIndex].answers[buttonIndex];
@@ -51,7 +177,7 @@ const clickHandler = (e) =>{
         window.alert('正解!');
         score++;
     }else{
-        window.alert('不正解!');
+        window.alert('不正解!答えは\n'+quiz[quizIndex].correct);
     }
 
     quizIndex++;
